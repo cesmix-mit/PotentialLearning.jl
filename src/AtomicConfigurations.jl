b4a7dcd219c9b818ef1d1204cc0ea6858c2c6fba
Emmanuel Lujan <lujan.emmanuel@gmail.com>
2021-07-04 02:57:16 -0400

    small changes in input loading

--- src/AtomicConfigurations.jl
+++ src/AtomicConfigurations.jl
@@ -1,3 +1,29 @@
+# Load configuration parameters
+function load_conf_params(path)
+    @show path
+    params = Dict()
+    open(string(path, "/PotentialLearning.conf")) do f
+        while !eof(f)
+            line = split(readline(f))
+            lhs = line[1]
+            if lhs == "rcut"
+                rhs = parse(Float64, line[2])
+            elseif lhs == "DFT_model"
+                rhs = line[2]
+            elseif length(line[2:end]) == 1
+                rhs = parse(Int64, line[2])
+            else
+                rhs = parse.(Int64, line[2:end])
+            end
+            params[lhs] = rhs
+        end
+    end 
+    params["positions_per_conf"] = load_positions_per_conf(path,
+                                                    params["no_atoms_per_conf"],
+                                                    params["no_atomic_conf"])
+    return params
+end
+
 # Read atomic positions per configuration
 function load_positions_per_conf(path, no_atoms_per_conf, no_atomic_conf)
     @show no_atoms_per_conf, no_atomic_conf
--- src/PotentialLearning.jl
+++ src/PotentialLearning.jl
@@ -26,31 +26,6 @@ function fit(p)
     p.β = p.A \ p.b
 end
 
-function load_conf_params(path)
-    @show path
-    params = Dict()
-    open(string(path, "/PotentialLearning.conf")) do f
-        while !eof(f)
-            line = split(readline(f))
-            lhs = line[1]
-            if lhs == "rcut"
-                rhs = parse(Float64, line[2])
-            elseif lhs == "DFT_model"
-                rhs = line[2]
-            elseif length(line[2:end]) == 1
-                rhs = parse(Int64, line[2])
-            else
-                rhs = parse.(Int64, line[2:end])
-            end
-            params[lhs] = rhs
-        end
-    end 
-    params["positions_per_conf"] = load_positions_per_conf(path,
-                                                    params["no_atoms_per_conf"],
-                                                    params["no_atomic_conf"])
-    return params
-end
-
 function run(path)
     # Currently this function is hardcoded to run a SNAP-LAMMPS example
 
