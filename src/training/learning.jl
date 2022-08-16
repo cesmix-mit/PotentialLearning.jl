9c280afad2be2c41f36f59f822a84dc1f18c8594
Emmanuel Lujan <lujan.emmanuel@gmail.com>
2022-08-16 15:49:30 -0400

    Improvements in training functions.

--- src/training/learning.jl
+++ src/training/learning.jl
@@ -44,25 +44,25 @@ end
 
 
 """
-    batch_train_opt!(ps, re, opt, maxiters, train_loader_e, train_loader_f,
-                     w_e, w_f, epoch, train_losses_batches)
+    batch_train_opt!(train_loader_e, train_loader_f, w_e, w_f, 
+                     ps, re, epoch, opt, maxiters, train_losses_batches)
 
-`ps`: neural network parameters. See Flux.destructure.
-`re`: neural network restructure. See Flux.destructure.
-`opt`: optimizer.
-`maxiters`: maximum number of iterations in the optimizer.
 `train_loader_e`: energy data loader.
 `train_loader_f`: force data loader.
 `w_e`: energy weight.
 `w_f`: force weight.
+`ps`: neural network parameters. See Flux.destructure.
+`re`: neural network restructure. See Flux.destructure.
 `epoch`: current epoch.
+`opt`: optimizer.
+`maxiters`: maximum number of iterations in the optimizer.
 `train_losses_batches`: vector of batch losses during training.
 
 Batch training using Optimization.jl. Returns NN parameters.
 
 """
-function batch_train_opt!(ps, re, opt, maxiters, train_loader_e, train_loader_f,
-                          w_e, w_f, epoch, train_losses_batches)
+function batch_train_opt!(train_loader_e, train_loader_f, w_e, w_f,
+                          ps, re, epoch, opt, maxiters, train_losses_batches)
     i = 1
     for ((bs_e, es), (bs_f, dbs_f, fs)) in zip(train_loader_e, train_loader_f)
         batch_loss(ps, p) = loss(potential_energy.(bs_e, [ps], [re]), es, w_e,
@@ -83,65 +83,62 @@ function batch_train_opt!(ps, re, opt, maxiters, train_loader_e, train_loader_f,
 end
 
 """
-    batch_train_flux!(ps, re, opt, maxiters, train_loader_e, train_loader_f, w_e, w_f)
+    batch_train_flux!(train_loader_e, train_loader_f, w_e, w_f, ps, re, opt)
 
-`ps`: neural network parameters. See Flux.destructure.
-`re`: neural network restructure. See Flux.destructure.
-`opt`: optimizer.
-`maxiters`: maximum number of iterations in the optimizer.
 `train_loader_e`: energy data loader.
 `train_loader_f`: force data loader.
 `w_e`: energy weight.
 `w_f`: force weight.
+`ps`: neural network parameters. See Flux.destructure.
+`re`: neural network restructure. See Flux.destructure.
+`opt`: optimizer.
 
 Batch training using Flux.jl. Returns neural network parameters.
 
 """
-function batch_train_flux!(ps, re, opt, maxiters, train_loader_e, train_loader_f, w_e, w_f)
+function batch_train_flux!(train_loader_e, train_loader_f, w_e, w_f, ps, re, opt)
     for ((bs_e, es), (bs_f, dbs_f, fs)) in zip(train_loader_e, train_loader_f)
         g = gradient(Flux.params(ps)) do
             loss(potential_energy.(bs_e, [ps], [re]), es, w_e,
                  force.(bs_f, dbs_f, [ps], [re]), fs, w_f)
         end
-        Flux.Optimise.update!(opt, Flux.params(ps), g, maxiters=maxiters)
+        Flux.Optimise.update!(opt, Flux.params(ps), g)
     end
     return ps
 end
 
 """
-    train!( lib, nnbp, epochs, opt, maxiters, train_loader_e, train_loader_f,
-            test_loader_e, test_loader_f, w_e, w_f)
+    train!( train_loader_e, train_loader_f, test_loader_e, test_loader_f,
+            w_e, w_f, nnbp, epochs, opt, maxiters)
 
-`lib`:
-`nnbp`:  
-`epochs`: no. of epochs.
-`opt`: optimizer.
-`maxiters`: maximum number of iterations in the optimizer.
 `train_loader_e`: energy data loader for training.
 `train_loader_f`: force data loader for training.
 `test_loader_e`: energy data loader for test.
 `test_loader_f`: force data loader for test.
 `w_e`: energy weight.
 `w_f`: force weight.
+`nnbp`:  neural network basis potential.
+`epochs`: no. of epochs.
+`opt`: optimizer.
+`maxiters`: maximum number of iterations in the optimizer.
 
 Train neural network potential.
 Returns losses of training and test per epoch and per batch.
 
 """
-function train!( lib, nnbp, epochs, opt, maxiters, train_loader_e, train_loader_f,
-                 test_loader_e, test_loader_f, w_e, w_f)
+function train!( train_loader_e, train_loader_f, test_loader_e, test_loader_f,
+                 w_e, w_f, nnbp, epochs, opt, maxiters = Infinite)
 
     train_losses_epochs = []; test_losses_epochs = []; train_losses_batches = []
     ps, re = Flux.destructure(nnbp.nn)
     for epoch in 1:epochs
-    
-        # TODO: use multiple dispatch here?
-        if lib == "Flux.jl"
-            ps = batch_train_flux!(ps, re, opt, maxiters, train_loader_e,
-                                   train_loader_f, w_e, w_f, epoch, train_losses_batches)
-        else # lib == "Optimization.jl"
-            ps = batch_train_opt!(ps, re, opt, maxiters, train_loader_e,
-                                  train_loader_f, w_e, w_f, epoch, train_losses_batches)
+        
+        if typeof(opt) == typeof(BFGS()) # use Optimization.jl
+            ps = batch_train_opt!(train_loader_e, train_loader_f, w_e, w_f,
+                                  ps, re, epoch, opt, maxiters, train_losses_batches)
+        else # use Flux.jl 
+            ps = batch_train_flux!(train_loader_e, train_loader_f, w_e, w_f, 
+                                   ps, re, opt)
         end
         
         # Report losses
@@ -149,9 +146,9 @@ function train!( lib, nnbp, epochs, opt, maxiters, train_loader_e, train_loader_
         test_loss = global_loss(test_loader_e, test_loader_f, w_e, w_f, ps, re)
         push!(train_losses_epochs, train_loss)
         push!(test_losses_epochs, test_loss)
-        println("Epoch $(epoch). Losses of complete datasets: \
-                 training loss: $(train_loss), \
-                 test loss: $(test_loss).")
+        println("Epoch $(epoch). \
+                 Training loss: $(train_loss). \
+                 Test loss: $(test_loss).")
     end
     nnbp.nn = re(ps)
     nnbp.nn_params = Flux.params(nnbp.nn)
@@ -159,24 +156,6 @@ function train!( lib, nnbp, epochs, opt, maxiters, train_loader_e, train_loader_
     return train_losses_epochs, test_losses_epochs, train_losses_batches
 end
 
-
-# Training using BFGS from Optimizer.jl (1 batch)
-#time_fitting += Base.@elapsed begin
-#    ps, re = Flux.destructure(nnbp.nn)
-#    ((bs_e, es), (bs_f, dbs_f, fs)) = collect(zip(train_loader_e, train_loader_f))[1]
-#    loss(ps, p) = loss(potential_energy.(bs_e, [ps], [re]), es, 
-#                       force.(bs_f, dbs_f, [ps], [re]), fs)
-#    dlossdps = OptimizationFunction(loss, Optimization.AutoForwardDiff()) # Optimization.AutoZygote()
-#    prob = OptimizationProblem(dlossdps, ps, []) #prob = remake(prob,u0=sol.minimizer)
-#    callback = function (p, l)
-#        println("Thread: $(Threads.threadid()) current loss is: $l")
-#        return false
-#    end
-#    sol = solve(prob, BFGS(), callback=callback, maxiters=1000) # reltol = 1e-14
-#    ps = sol.u
-#    nnbp.nn = re(ps)
-#    nnbp.nn_params = Flux.params(nnbp.nn)
-#end
 
 #-------------------------------------------------------------------------------
 ## Training using BFGS and data parallelism with Base.Threads
