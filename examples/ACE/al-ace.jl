b354d70f7ca83d75ee85168684e621c8e421e290
Emmanuel Lujan <lujan.emmanuel@gmail.com>
2023-02-02 16:28:00 -0500

    active learning ace bis

--- examples/ACE/al-ace.jl
+++ examples/ACE/al-ace.jl
@@ -11,12 +11,12 @@ using StaticArrays
 include("utils.jl")
 
 # Load input parameters
-args = ["experiment_path",      "a-Hfo2-300K-NVT-6000-37/",
+args = ["experiment_path",      "AL-a-Hfo2-300K-NVT-6000/",
         "dataset_path",         "../../../data/",
         "dataset_filename",     "a-Hfo2-300K-NVT-6000.extxyz",
         "random_seed",          "100",  # Random seed to ensure reproducibility of loading and subsampling.
-        "n_train_sys",          "100",  # Training dataset size
-        "n_test_sys",           "100",  # Test dataset size
+        "n_train_sys",          "10",  # Training dataset size
+        "n_test_sys",           "10",  # Test dataset size
         "n_body",               "3",
         "max_deg",              "4",
         "r0",                   "1.0",
@@ -60,11 +60,12 @@ end
 pos = get_positions.(ds_train)
 flat_pos = [ reduce(vcat, get_values(p)) for p in pos ]
 X = reduce(hcat, flat_pos)
-R = kmeans(X, 10)
+n_clusters = 3 
+R = kmeans(X, 3)
 a = assignments(R) # get the assignments of points to clusters
 c = counts(R) # get the cluster sizes
 M = R.centers # get the cluster centers
-clusters = [findall(x->x==i, a) for i in 1:10]
+clusters = [findall(x->x==i, a) for i in 1:n_clusters]
 
 # Define ACE parameters
 species = unique(atomic_symbol(get_system(ds[1])))
@@ -77,12 +78,12 @@ rcutoff = input["rcutoff"]
 ace_basis = ACE(species, body_order, polynomial_degree, wL, csp, r0, rcutoff)
 @savevar path ace_basis
 
-function learn_old!(lp)
+function learn_old!(lp, ds)
     w_e = 1; w_f = 1
     
     B_train = reduce(hcat, lp.B)'
     dB_train = reduce(hcat, lp.dB)'
-    e_train, f_train = get_all_energies(ds_train), get_all_forces(ds_train)
+    e_train, f_train = get_all_energies(ds), get_all_forces(ds)
     
     # Calculate A and b.
     A = [B_train; dB_train]
@@ -100,21 +101,25 @@ end
 curr_e_descr_train = []
 curr_f_descr_train = []
 curr_ds_train = []
-while e_train_mae > 1.0 || f_train_mae > 1.0 
+ds_train_0 = []
+e_train_mae = f_train_mae = 100.0
+B_time = dB_time = learn_time = 0.0
+while e_train_mae > 10.0 || f_train_mae > 10.0 
 
     # Sample from clusters
-    inds = reduce(vcat, [sample(c, 5) for c in clusters])
-    new_ds_train = ds_train[inds]
+    inds = reduce(vcat, [sample(c, 2) for c in clusters])
+    global new_ds_train = ds_train[inds]
     
     # Update training dataset by adding energy and force descriptors
     println("Computing energy descriptors of training dataset: ")
-    B_time = @elapsed begin
+    new_e_descr_train = new_f_descr_train = [] 
+    global B_time = @elapsed begin
         new_e_descr_train = [LocalDescriptors(compute_local_descriptors(sys, ace_basis)) 
                                           for sys in ProgressBar(get_system.(new_ds_train))]
     end
 
     println("Computing force descriptors of training dataset: ")
-    dB_time = @elapsed begin
+    global dB_time = @elapsed begin
         new_f_descr_train = [ForceDescriptors([[fi[i, :] for i = 1:3]
                                            for fi in compute_force_descriptors(sys, ace_basis)])
                          for sys in ProgressBar(get_system.(new_ds_train))]
@@ -123,22 +128,22 @@ while e_train_mae > 1.0 || f_train_mae > 1.0
     push!(curr_e_descr_train, new_e_descr_train...)
     push!(curr_f_descr_train, new_f_descr_train...)
     if length(curr_ds_train) > 0
-        curr_ds_train = curr_ds_train .+ new_ds_train
+        global curr_ds_train = curr_ds_train .+ new_ds_train
     else
-        curr_ds_train = new_ds_train
+        global curr_ds_train = new_ds_train
     end
-    ds_train = DataSet(curr_ds_train .+ curr_e_descr_train .+ curr_f_descr_train)
+    ds_train_0 = DataSet(curr_ds_train .+ curr_e_descr_train .+ curr_f_descr_train)
 
     # Learn
     println("Learning energies and forces...")
-    learn_time = @elapsed begin
-        lp = LinearProblem(ds_train)
-        learn_old!(lp)
+    global learn_time = @elapsed begin
+        lp = LinearProblem(ds_train_0)
+        learn_old!(lp, ds_train_0)
     end
 
     # Get true and predicted values
-    e_train, f_train = get_all_energies(ds_train), get_all_forces(ds_train)
-    e_train_pred, f_train_pred = get_all_energies(ds_train, lp), get_all_forces(ds_train, lp)
+    e_train, f_train = get_all_energies(ds_train_0), get_all_forces(ds_train_0)
+    e_train_pred, f_train_pred = get_all_energies(ds_train_0, lp), get_all_forces(ds_train_0, lp)
     
     # Compute metrics
     e_train_mae, e_train_rmse, e_train_rsq = calc_metrics(e_train_pred, e_train)
@@ -162,9 +167,9 @@ ds_test = DataSet(ds_test .+ e_descr_test .+ f_descr_test)
 
 
 # Get true and predicted values
-e_train, f_train = get_all_energies(ds_train), get_all_forces(ds_train)
+e_train, f_train = get_all_energies(ds_train_0), get_all_forces(ds_train_0)
 e_test, f_test = get_all_energies(ds_test), get_all_forces(ds_test)
-e_train_pred, f_train_pred = get_all_energies(ds_train, lp), get_all_forces(ds_train, lp)
+e_train_pred, f_train_pred = get_all_energies(ds_train_0, lp), get_all_forces(ds_train_0, lp)
 e_test_pred, f_test_pred = get_all_energies(ds_test, lp), get_all_forces(ds_test, lp)
 @savevar path e_train
 @savevar path e_train_pred
