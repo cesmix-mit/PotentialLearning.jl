1aaa824a7c4e718e319fe135a8bd5e681983c424
joannajzou <joannajzou@gmail.com>
2023-04-12 18:57:21 -0400

    initiate linear_tests
    Tests the learn!() function for linear problems in Learning/linear.jl. At this stage, it only tests the types/values of elements of a UnivariateLinearProblem.

---
+++ test/learning/linear_tests.jl
@@ -0,0 +1,53 @@
+using LinearAlgebra
+using AtomsBase, Unitful, UnitfulAtomic, StaticArrays
+using InteratomicPotentials, InteratomicBasisPotentials
+using ACE1, JuLIP
+
+# load data for test
+# ds, thermo = load_data("test/learning/liquify_sodium.yaml", YAML(:Na, u"eV", u"Å"))
+
+
+# initialize some fake descriptors
+d = 8
+num_atoms = 20
+num_configs = 10
+ld_0 = [[randn(d) for i = 1:num_atoms] for j = 1:num_configs]
+ld = LocalDescriptors.(ld_0)
+e = Energy.(rand(num_configs), (u"eV",))
+ds = DataSet(Configuration.(e, ld))
+
+
+# initialize a LinearBasisPotential
+n_body = 2  
+max_deg = 8 
+r0 = 1.0 
+rcutoff = 5.0 
+wL = 1.0 
+csp = 1.0 
+ace = ACE([:Na], n_body, max_deg, wL, csp, r0, rcutoff)
+lb = LBasisPotential(ace)
+
+
+# test input types
+lp = PotentialLearning.LinearProblem(ds)
+@test typeof(lp) <: PotentialLearning.LinearProblem
+@test typeof(lp) <: PotentialLearning.UnivariateLinearProblem
+@test typeof(ds) <: DataSet
+@test typeof(lb) <: InteratomicPotentials.LinearBasisPotential
+
+
+# test input values before learning
+@test lp.dv_data == get_values.(e)
+@test lp.iv_data == sum.(get_values.(get_local_descriptors.(ds)))
+@test lp.σ == [1.0]
+@test lp.β == zeros(d) # coeffs in LinearProblem
+@test lb.β == zeros(d) # coeffs in LinearBasisPotential
+
+
+# test learning functions
+# test learn!(lb::LinearBasisPotential, ds::DataSet; α::Real = 1e-8, return_cov = true)
+lb, Σ = learn!(lb, ds)
+# test learn!(lp::UnivariateLinearProblem; α = 1e-8) (internal method)
+lp = learn!(lp) 
+# test the two give the same output
+@test lb.β == lp.β
