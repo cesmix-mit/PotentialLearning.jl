0fbe906f4cd211742db086b4ad7c1c46a6747b8b
Emmanuel Lujan <lujan.emmanuel@gmail.com>
2023-05-19 00:25:28 -0400

    Changes in paths, new example readme, and adding new example dependency.

--- examples/Na/fit-dpp-ace-na.jl
+++ examples/Na/fit-dpp-ace-na.jl
@@ -1,18 +1,14 @@
-# To run this file:
-#    $ cd PotentialLearning.jl
-#    $ julia --project=./ --threads=4
-#    $ pkg> instantiate
-#    $ include("examples/Na/fit-dpp-ace-na.jl")
+push!(Base.LOAD_PATH, "../../")
 
 using AtomsBase
 using Unitful, UnitfulAtomic
-using InteratomicPotentials 
+using InteratomicPotentials
 using InteratomicBasisPotentials
 using PotentialLearning
+#using JLD
 
 # Load dataset
-confs, thermo = load_data("examples/Na/data/liquify_sodium.yaml",
-                           YAML(:Na, u"eV", u"Å"))
+confs, thermo = load_data("data/liquify_sodium.yaml", YAML(:Na, u"eV", u"Å"))
 confs, thermo = confs[220:end], thermo[220:end]
 
 # Split dataset
@@ -20,8 +16,8 @@ conf_train, conf_test = confs[1:1000], confs[1001:end]
 
 # Define ACE
 ace = ACE(species = [:Na],         # species
-          body_order = 2,          # 4-body
-          polynomial_degree = 3,   # 8 degree polynomials
+          body_order = 4,          # 4-body
+          polynomial_degree = 8,   # 8 degree polynomials
           wL = 1.0,                # Defaults, See ACE.jl documentation 
           csp = 1.0,               # Defaults, See ACE.jl documentation 
           r0 = 1.0,                # minimum distance between atoms
@@ -29,7 +25,7 @@ ace = ACE(species = [:Na],         # species
 
 # Update training dataset by adding energy (local) descriptors
 e_descr_train = compute_local_descriptors(conf_train, ace)
-# e_descr_train = JLD.load("examples/Na/data/sodium_empirical_full.jld", "descriptors")
+#e_descr_train = JLD.load("data/sodium_empirical_full.jld", "descriptors")
 ds_train = DataSet(conf_train .+ e_descr_train)
 
 # Learn, using DPP
--- examples/README.md
+++ examples/README.md
@@ -1,1 +1,21 @@
-See https://github.com/cesmix-mit/AtomisticComposableWorkflows
+# How to run an example
+
+Change the directory to the desired example folder. E.g.
+```bash
+$ cd PotentialLearning.jl/examples/Na
+```
+
+Open Julia REPL, activating ```Project.toml``` file in folder ```examples```, and chosing the number of threads.
+```bash
+$ julia --project=.. --threads=4
+```
+
+Instantiate: type ```]``` in Julia REPL, and then run ```instantiate```.
+```julia
+    pkg> instantiate
+```
+
+Include example script. E.g.
+```julia
+    julia> include("fit-dpp-ace-na.jl")
+```
