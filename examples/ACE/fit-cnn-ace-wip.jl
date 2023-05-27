cfec281830d2add2f8d01a46c3e2267f435381a8
Emmanuel Lujan <lujan.emmanuel@gmail.com>
2023-05-27 14:33:13 -0400

    Progress on CNN-ACE.

--- examples/ACE/fit-cnn-ace-wip.jl
+++ examples/ACE/fit-cnn-ace-wip.jl
@@ -24,18 +24,19 @@ args = ["experiment_path",      "a-Hfo2-300K-NVT-6000-CNN-ACE/",
         "random_seed",          "100",
         "n_train_sys",          "100",
         "n_test_sys",           "100",
-        "nn",                   "Chain(Dense(n_desc,8,relu),Dense(8,1))",
-        "n_epochs",             "100",
-        "n_batches",            "1",
-        "optimiser",            "Adam(0.001)", # e.g. Adam(0.01) or BFGS()
+#        "nn",                   "Chain(Dense(n_desc,8,relu),Dense(8,1))",
+#        "n_epochs",             "100",
+#        "n_batches",            "1",
+#        "optimiser",            "Adam(0.001)", # e.g. Adam(0.01) or BFGS()
         "n_body",               "3",
         "max_deg",              "3",
         "r0",                   "1.0",
         "rcutoff",              "5.0",
         "wL",                   "1.0",
-        "csp",                  "1.0",
-        "w_e",                  "1.0",
-        "w_f",                  "1.0"]
+        "csp",                  "1.0"
+#        "w_e",                  "1.0",
+#        "w_f",                  "1.0"
+        ]
 args = length(ARGS) > 0 ? ARGS : args
 input = get_input(args)
 
@@ -81,31 +82,48 @@ ds_train = DataSet(conf_train .+ e_descr_train .+ f_descr_train)
 n_desc = length(e_descr_train[1][1])
 
 # Pre-process data
-xs = []
-for c in ds_train
-    ld_c = reduce(hcat, get_values(get_local_descriptors(c)))'
-    ld_c = cat( ld_c[:, 1:n_desc÷2], ld_c[:, n_desc÷2+1:end], dims=3 )
-    if xs == []
-        xs = ld_c
-    else
-        xs = cat( xs, ld_c, dims=4)
+function get_e_descr_batch(ds)
+    xs = []
+    for c in ds
+        ld_c = reduce(hcat, get_values(get_local_descriptors(c)))'
+        ld_c = ld_c[randperm(size(ld_c,1)),:]
+        ld_c = cat( ld_c[:, 1:n_desc÷2], ld_c[:, n_desc÷2+1:end], dims=3 )
+        if xs == []
+            xs = ld_c
+        else
+            xs = cat(xs, ld_c, dims=4)
+        end
     end
+    return xs
 end
-ys = get_all_energies(ds_train)
 
 # Define neural network model
-(n_atoms, n_basis, n_types, batch_size) = size(xs)
+n_atoms = length(get_system(first(ds_train)))
+n_types = length(ace.species)
+n_basis = length(first(get_values(get_local_descriptors(first(ds_train))))) ÷ n_types
+batch_size = length(ds_train)
+#nn = Flux.@autosize (n_atoms, n_basis, n_types, batch_size) Chain(
+#    Conv((3, 3), 2=>6, relu),
+#    MaxPool((2, 2)),
+#    Conv((3, 3), _=>16, relu),
+#    MaxPool((2, 2)),
+#    Flux.flatten,
+#    Dense(_ => 120, relu),
+#    Dense(_ => 84, relu), 
+#    Dense(_ => 1),
+#)
+
 nn = Flux.@autosize (n_atoms, n_basis, n_types, batch_size) Chain(
-    Conv((3, 3), 2=>6, relu),
-    MaxPool((2, 2)),
-    Conv((3, 3), _=>16, relu),
-    MaxPool((2, 2)),
+    Conv((1, 3), 2=>6, relu),
+    MaxPool((1, 2)),
+    Conv((1, 3), _=>16, relu),
+    MaxPool((1, 2)),
     Flux.flatten,
     Dense(_ => 120, relu),
     Dense(_ => 84, relu), 
     Dense(_ => 1),
 )
-#nace = NNIAP(nn, ace)
+cnnace = NNIAP(nn, ace)
 
 # Learn
 println("Learning energies and forces...")
@@ -114,22 +132,80 @@ println("Learning energies and forces...")
 #n_epochs = input["n_epochs"]
 #learn!(nace, ds_train, opt, n_epochs, loss, w_e, w_f)
 
-η = 3e-4         # learning rate
+function loss(x, y)
+    return Flux.mse(x, y)
+end
+
+function learn!(cnnnace, ds_train, opt, n_epochs, loss)
+    es = get_all_energies(ds_train) |> gpu
+    ld = get_e_descr_batch(ds_train) |> gpu
+    nn = cnnnace.nn |> gpu
+    opt = opt |> gpu
+    for epoch in 1:n_epochs
+        grads = Flux.gradient(m -> loss(m(ld)', es), nn)
+        Flux.update!(opt, nn, grads[1])
+        if epoch % 100 == 0
+            train_loss = loss(nn(ld)', es)
+            println("epoch = $epoch; loss = $train_loss")
+        end
+    end
+    cnnnace.nn = nn |> cpu
+end
+
+η = 1e-3         # learning rate
 λ = 1e-2         # for weight decay
-epochs = 100_000
 opt_rule = OptimiserChain(WeightDecay(λ), Adam(η))
 opt_state = Flux.setup(opt_rule, nn)
+n_epochs = 10_000
+learn!(cnnace, ds_train, opt_state, n_epochs, loss)
 
-for epoch in 1:epochs
-    grads = Flux.gradient(m -> Flux.mse(m(xs)', ys), nn)
-    Flux.update!(opt_state, nn, grads[1])
-    if epoch % 100 == 0
-        train_loss = Flux.mse(nn(xs)', ys)
-        println("epoch = $epoch; loss = $train_loss")
-    end
-end
+η = 1e-5         # learning rate
+λ = 1e-3         # for weight decay
+opt_rule = OptimiserChain(WeightDecay(λ), Adam(η))
+opt_state = Flux.setup(opt_rule, nn)
+n_epochs = 10_000
+learn!(cnnace, ds_train, opt_state, n_epochs, loss)
+
 
 end # end of "learn_time = @elapsed begin"
 
-@savevar path Flux.params(nace.nn)
+@savevar path Flux.params(cnnace.nn)
+
+
+# Post-process output: calculate metrics, create plots, and save results
+
+# Update test dataset by adding energy and force descriptors
+println("Computing energy descriptors of test dataset...")
+e_descr_test = compute_local_descriptors(conf_test, ace, T = Float32)
+println("Computing force descriptors of test dataset...")
+f_descr_test = compute_force_descriptors(conf_test, ace, T = Float32)
+GC.gc()
+ds_test = DataSet(conf_test .+ e_descr_test .+ f_descr_test)
+
+
+function get_all_energies(ds::DataSet, nniap::NNIAP)
+    return nniap.nn(get_e_descr_batch(ds))'
+end
+
+# Get true and predicted values
+e_train = get_all_energies(ds_train)
+e_test = get_all_energies(ds_test)
+e_train_pred = get_all_energies(ds_train, cnnace)
+e_test_pred = get_all_energies(ds_test, cnnace)
+@savevar path e_train
+@savevar path e_train_pred
+@savevar path e_test
+@savevar path e_test_pred
+
+# Compute metrics
+metrics = calc_metrics(e_train_pred, e_train)
+metrics = calc_metrics(e_test_pred, e_test)
+#@savecsv path metrics
+
+# Plot and save results
+e_train_plot = plot_energy(e_train_pred, e_train)
+@savefig path e_train_plot
+e_test_plot = plot_energy(e_test_pred, e_test)
+@savefig path e_test_plot
+
 
