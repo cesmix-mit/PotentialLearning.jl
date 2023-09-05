d8fe24c75bb25229898b335fd8c24e353a59fd9f
Emmanuel Lujan <lujan.emmanuel@gmail.com>
2023-09-05 14:04:04 -0400

    Renaming of MLE file to OLS file. Modification of Learning.jl properly include updated files.

--- src/Learning/learning.jl
+++ src/Learning/learning.jl
@@ -5,10 +5,9 @@ export learn!, get_all_energies, get_all_forces, LBasisPotentialExt
 include("InteratomicBasisPotentialsExtension.jl")
 
 include("general-learning-problem.jl")
-include("general-learn.jl")
 
 include("linear-learning-problem.jl")
-include("mle-linear-learn.jl")
+include("ols-linear-learn.jl")
 include("wls-linear-learn.jl") # Default learning algorithms
 
 include("utils.jl")
