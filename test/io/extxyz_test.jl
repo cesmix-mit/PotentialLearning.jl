d65b6a4b86c307a01d7c1dfe841cdfec954bd7d1
Emmanuel Lujan <lujan.emmanuel@gmail.com>
2023-09-07 20:11:10 -0400

    New data path in IO tests.

--- test/io/extxyz_test.jl
+++ test/io/extxyz_test.jl
@@ -3,7 +3,7 @@ using Unitful, UnitfulAtomic
 
 energy_units = u"eV"
 distance_units = u"Å"
-ds = load_data("../examples/Si-3Body-LAMMPS/data.xyz", ExtXYZ(energy_units, distance_units));
+ds = load_data("../examples/data/Si-3Body-LAMMPS/data.xyz", ExtXYZ(energy_units, distance_units));
 
 @test length(ds) == 201
 @test typeof(ds) == DataSet
