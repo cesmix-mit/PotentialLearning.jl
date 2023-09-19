6ed5c8cf2c02664e1127e692fde703a80dc17ba3
Emmanuel Lujan <lujan.emmanuel@gmail.com>
2023-09-19 13:08:05 -0400

    Adding new subselection method based on DBSCAN clustering.

--- src/SubsetSelection/subsetselector.jl
+++ src/SubsetSelection/subsetselector.jl
@@ -2,6 +2,7 @@ abstract type SubsetSelector end
 
 include("dpp.jl")
 include("random.jl")
+include("dbscan.jl")
 # include("hdbscan.jl")
 export SubsetSelector, kDPP, get_random_subset, get_dpp_mode, get_inclusion_prob
 export RandomSelector
