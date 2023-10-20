afc0dbe833216c0114c0239b6ea9b0318692d63a
joannajzou <joannajzou@gmail.com>
2023-10-20 12:35:34 -0400

    add get_all_forces_mag function
    Add a function in Data/utils.jl which returns the magnitude of force acting on each atom in each configuration.
    
    Unlike get_all_forces(), this function separates data in the form Vector{Vector{<:Real}} where the inner Vector has natom components and the outer Vector has nconfig components.

--- src/Data/utils.jl
+++ src/Data/utils.jl
@@ -55,6 +55,38 @@ function get_all_forces(
     return vcat([lb.β0[1] .+  dB' * lb.β for dB in [reduce(hcat, fi) for fi in force_descriptors]]...)
 end
 
+"""
+function get_all_forces_mag(
+    ds::DataSet,
+)
+
+Returns the force magnitudes acting on each atom in each configuration.
+"""
+function get_all_forces_mag(
+    ds::DataSet,
+)
+    return reduce(vcat, [norm.(get_values(get_forces(ds[c]))) for c in 1:length(ds)])
+end
+
+
+"""
+function get_all_forces_mag(
+    ds::DataSet,
+    lb::LinearBasisPotential
+)
+
+Returns the force magnitudes acting on each atom in each configuration.
+"""
+function get_all_forces_mag(
+    ds::DataSet,
+    lb::LinearBasisPotential
+)
+    force_descriptors = [reduce(vcat, get_values(get_force_descriptors(dsi)) ) for dsi in ds]
+    force_pred = [lb.β0[1] .+  dB' * lb.β for dB in [reduce(hcat, fi) for fi in force_descriptors]]
+    return reduce(vcat, [norm.([f[k:k+2] for k = 1:3:length(f)]) for f in force_pred])
+end
+
+
 # Compute local and force descriptors ##########################################
 
 """
