511d2a802b1ed55acf88749354d9f19ddead079d
joannajzou <joannajzou@gmail.com>
2023-10-23 23:16:28 -0400

    refactor subsampling scripts
    Refactor of subsampling experiments to iterate over sets of ACE potential parameters (body order, degree).
    
    For each set of parameters, 5-fold cross validation training is performed using a) DPP subsampling and b) simple random sampling for a number of batch sizes.
    
    Fitted potentials are evaluated based on MAE, RMSE, and RSQ metrics for predicted energies and forces. Statistics (mean, variance) over the metrics is computed from the k-fold cross validation results.
    
    The script is prepared for running in parallel using @async.
    
    All relevant functions are under "subsampling_utils.jl".

--- examples/DFT-subsampling/subsampling_utils.jl
+++ examples/DFT-subsampling/subsampling_utils.jl
@@ -1,3 +1,275 @@
+# master function for iteration ------------------------------------------------------------------
+
+function DPP_training_trial(
+    elem::Vector,
+    confs::DataSet,
+    nbody::Int64,
+    deg::Int64,
+    batch_size::Vector,
+    files::Vector{String},
+    confs::Vector{DataSet},
+    save_dir::String;
+    nfold::Int64=10)
+
+    # Define directory
+    exp_dir = "$(save_dir)/ACE_$(nbody)body_$(deg)deg/"
+    run(`mkdir -p $exp_dir`)
+
+    # Define ACE basis
+    ace = ACE(species = elem,             # species
+            body_order = nbody,           # n-body
+            polynomial_degree = deg,      # degree of polynomials
+            wL = 1.0,                     # Defaults, See ACE.jl documentation 
+            csp = 1.0,                    # Defaults, See ACE.jl documentation 
+            r0 = 1.0,                     # minimum distance between atoms
+            rcutoff = 10.0)
+
+    # Update dataset by adding energy (local) descriptors
+    println("Computing local descriptors")
+    @time e_descr = compute_local_descriptors(confs, ace)
+    @time f_descr = compute_force_descriptors(confs, ace)
+    JLD.save(exp_dir*"energy_descriptors.jld", "e_descr", e_descr)
+    JLD.save(exp_dir*"force_descriptors.jld", "f_descr", f_descr)
+
+    # e_descr = JLD.load(outpath*"$(elname)_energy_descriptors.jld")["e_descr"]
+    # f_descr = JLD.load(outpath*"$(elname)_force_descriptors.jld")["f_descr"]
+
+    ds = DataSet(confs .+ e_descr .+ f_descr)
+    ndata = length(ds)
+
+    # Compute cross validation error from training
+    res = cross_validation_training(ds, batch_size, exp_dir; ndiv=nfold)
+    df = compute_cv_metadata(ds, res, files, confs, exp_dir)
+
+end
+
+
+# helper functions for training potential ------------------------------------------------------------------
+
+# train using DPP sampling
+function train_potential(
+    ds::DataSet,
+    ace::ACE,
+    L::EllEnsemble,
+    batch_size::Int64;
+    α=1e-8)
+
+    # init basis potential
+    lb = LBasisPotentialExt(ace)
+    # compute kDPP
+    rescale!(L, batch_size)
+    dpp = kDPP(L, batch_size)
+    # draw subset
+    inds = get_random_subset(dpp)
+    # learning problem
+    lp = learn!(lb, ds[inds], α)
+
+    return lp, lb, inds
+end 
+
+
+# train using simple random sampling
+function train_potential(
+    ds::DataSet,
+    ace::ACE,
+    batch_size::Int64;
+    α=1e-8)
+
+    # init basis potential
+    lb = LBasisPotentialExt(ace)
+    # define random selector
+    srs = RandomSelector(length(ds), batch_size)
+    # draw subset
+    inds = get_random_subset(srs)
+    # learning problem
+    lp = learn!(lb, ds[inds], α)
+
+    return lp, lb, inds
+end 
+
+
+# cross validation training
+function cross_validation_training(
+    ds::DataSet,
+    batch_size::Vector,
+    save_dir::String;
+    ndiv=10,
+    )
+    
+    # init results dict
+    res_srs = init_res_dict(batch_size, ndiv)
+    res_dpp = init_res_dict(batch_size, ndiv)
+
+    # make random divisions of data
+    ndata = length(ds)
+    ncut = Int(floor(ndata / ndiv))
+    ind_all = rand(1:ndata, ndata)
+    ind = [ind_all[(k*ncut+1):((k+1)*ncut)] for k = 0:(ndiv-1)]
+
+    # iterate over divisions
+    for i = 1:ndiv
+        println("batch $i")
+
+        # split train set
+        train_ind = reduce(vcat, ind[Not(i)]) # leave out one cut
+        ds_train = ds[train_ind]
+
+        # compute L-ensemble for DPP once
+        L = compute_ell_ensemble(ds_train, GlobalMean(), DotProduct())
+
+        for bs in batch_size
+            # train by DPP
+            lpd ,lbd, dpp_ind = train_potential(ds_train, ace, L, bs)
+            res_dpp[bs] = update_res_dict(i, res_dpp[bs], ds, lpd, lbd, dpp_ind)
+
+            # train by simple random sampling
+            lps, lbs, srs_ind = train_potential(ds_train, ace, bs)
+            res_srs[bs] = update_res_dict(i, res_srs[bs], ds, lps, lbs, srs_ind)
+
+        end
+    end
+
+    JLD.save(save_dir*"DPP_training_results.jld", "res", res_dpp)
+    
+    return res
+end
+
+
+# compute condition number ------------------------------------------------------------------
+
+# for UnivariateLinearProblem
+function compute_cond_num(lp::PotentialLearning.UnivariateLinearProblem)
+    A = reduce(hcat, lp.iv_data)'
+    return cond(A)
+end
+
+# for CovariateLinearProblem
+function compute_cond_num(lp::PotentialLearning.CovariateLinearProblem)
+    B = reduce(hcat, lp.B)'
+    dB = reduce(hcat, lp.dB)'
+    A = [B; dB]
+    return cond(A)
+end
+
+
+function compute_ell_ensemble(
+    ds::DataSet,
+    f::Feature,
+    k::Kernel;
+    dt = LocalDescriptors,
+)
+    K = KernelMatrix(ds, f, k; dt = dt)
+    ell = EllEnsemble(K)
+    return ell
+end
+
+
+# get all force magnitudes ---------------------------------------------------------------------------
+
+# ground truth forces
+function get_all_forces_mag(ds::DataSet)
+    return reduce(vcat, [norm.(get_values(get_forces(ds[c]))) for c in 1:length(ds)])
+end
+
+# predicted forces
+function get_all_forces_mag(
+    ds::DataSet,
+    lb::PotentialLearning.LinearBasisPotential
+)
+    force_descriptors = [reduce(vcat, get_values(get_force_descriptors(dsi)) ) for dsi in ds]
+    force_pred = [lb.β0[1] .+  dB' * lb.β for dB in [reduce(hcat, fi) for fi in force_descriptors]]
+    return reduce(vcat, [norm.([f[k:k+2] for k = 1:3:length(f)]) for f in force_pred])
+end
+
+
+
+# record results -------------------------------------------------------------------------------------
+
+function init_res_dict(batch_size::Vector, ndiv::Int64)
+    res = Dict{Int64, Dict}(bs => Dict{String, Vector}() for bs in batch_size)
+    for bs in batch_size
+        res[bs]["cond_num"] = zeros(ndiv)
+        res[bs]["indices"] = Vector{Vector{Float64}}(undef, ndiv)
+        res[bs]["energy_err"] = Vector{Vector{Float64}}(undef, ndiv)
+        res[bs]["energy_mae"] = zeros(ndiv)
+        res[bs]["energy_rmse"] = zeros(ndiv)
+        res[bs]["energy_rsq"] = zeros(ndiv)
+        res[bs]["force_err"] = Vector{Vector{Float64}}(undef, ndiv)
+        res[bs]["force_mae"] = zeros(ndiv)
+        res[bs]["force_rmse"] = zeros(ndiv)
+        res[bs]["force_rsq"] = zeros(ndiv)
+    end
+    return res
+end
+
+
+function update_res_dict(
+    i::Int64,
+    res::Dict,
+    ds::DataSet,
+    lp,
+    lb::LBasisPotentialExt,
+    ind::Vector, 
+)
+    res["cond_num"][i] = compute_cond_num(lp)
+    res["indices"][i] = ind
+
+    # get DFT and predicted energies/forces
+    energies = get_all_energies(ds)
+    forces = get_all_forces_mag(ds) # magnitude
+    e_pred = get_all_energies(ds, lb)
+    f_pred = get_all_forces_mag(ds, lb)
+
+    # compute errors
+    res["energy_err"][i] = energies - e_pred 
+    res["force_err"][i] = forces - f_pred
+    res["energy_mae"][i], res["energy_rmse"][i], res["energy_rsq"][i] = calc_metrics(energies, e_pred)
+    res["force_mae"][i], res["force_rmse"][i], res["force_rsq"][i] = calc_metrics(forces, f_pred)
+    
+    return res
+end
+
+
+function compute_cv_metadata(
+    ds::DataSet,
+    res::Dict,
+    files::Vector{String},
+    confs::Vector{DataSet},
+    save_dir::String
+)
+    batch_size = keys(res)
+
+    for bs in batch_size
+        # populate DataFrame
+        df_conf = DataFrame("config" => 1:length(ds),
+            "file" => reduce(vcat, [[files[j] for i = 1:length(confs[j])] for j = 1:length(confs)]),
+            "DFT energy" => get_all_energies(ds),
+            "E err mean" => mean(res[bs]["energy_err"]), # mean error from k-fold CV
+            "E err std" => std(res[bs]["energy_err"]), # std of error
+            "E mae mean" => mean(res[bs]["energy_mae"]),
+            "E rmse mean" => mean(res[bs]["energy_rmse"]),
+            "E rsq mean" => mean(res[bs]["energy_rsq"]),
+            "DFT force" => get_all_forces_mag(ds),
+            "F err mean" =>  mean(res[bs]["force_err"]), # mean error from k-fold CV
+            "F err std" => std(res[bs]["force_err"]), # std of error
+            "F mae mean" => mean(res[bs]["force_mae"]),
+            "F rmse mean" => mean(res[bs]["force_rmse"]),
+            "F rsq mean" => mean(res[bs]["force_rsq"]),
+        )
+
+        CSV.write(save_dir*"training_metadata.csv", df_conf)
+
+        return df_conf
+    end
+end
+
+# write to file
+# CSV.write(outpath*"$(elname)_ACE-$(nbody)-$(deg)_train_full_nDPP=$(dpp_batch).csv", df_conf)
+# CSV.write(outpath*"$(elname)_ACE-$(nbody)-$(deg)_train_metadata_nDPP=$(dpp_batch).csv", df_meta)
+
+
+
+# I/O helper functions ----------------------------------------------------------------------
 function readext(path::String, ext::String)
     dir = readdir(path)
     substr = [split(f, ".") for f in dir]
@@ -11,124 +283,4 @@ function concat_dataset(confs::Vector{DataSet})
     confs_vec = [[confs[i][j] for j = 1:length(confs[i])] for i = 1:N]
     confs_all = reduce(vcat, confs_vec)
     return DataSet(confs_all)
-end
-
-
-function train_potential(ds_train::DataSet, ace::ACE, dpp_batch::Int64)
-    # learn with DPP
-    lb = LBasisPotentialExt(ace)
-    dpp = kDPP(ds_train, GlobalMean(), DotProduct(); batch_size = dpp_batch)
-    dpp_inds = get_random_subset(dpp)
-    lp = learn!(lb, ds_train[dpp_inds], [100, 1], false)
-
-    cond_num = compute_cond_num(lp)
-    return lb, cond_num, dpp_inds
-end 
-
-
-function compute_cond_num(lp::PotentialLearning.UnivariateLinearProblem)
-    A = reduce(hcat, lp.iv_data)'
-    return cond(A)
-end
-
-
-function compute_cond_num(lp::PotentialLearning.CovariateLinearProblem)
-    B = reduce(hcat, lp.B)'
-    dB = reduce(hcat, lp.dB)'
-    A = [B; dB]
-    return cond(A)
-end
-
-
-function cross_validation_training(ds; ndiv=10,
-    dpp_batch=Int(floor(2*length(ds) / ndiv))
-    )
-
-    # retrieve DFT data
-    energies = get_all_energies(ds)
-    forces = reduce(vcat,[sum(norm.(get_values(get_forces(ds[c]))))
-                for c in 1:length(ds)]) # magnitude
-    
-    # init arrays
-    cond_num = zeros(ndiv)
-    e_err = Matrix{Float64}(undef, (length(ds), ndiv))
-    f_err = Matrix{Float64}(undef, (length(ds), ndiv))
-    e_mae, e_rmse = Dict(f => zeros(ndiv) for f in file_arr), Dict(f => zeros(ndiv) for f in file_arr)
-    f_mae, f_rmse = Dict(f => zeros(ndiv) for f in file_arr), Dict(f => zeros(ndiv) for f in file_arr)
-    sel_ind = Vector{Vector{Int64}}(undef, ndiv)
-
-    # make random divisions of data
-    ndata = length(ds)
-    ncut = Int(floor(ndata / ndiv))
-    ind_all = rand(1:ndata, ndata)
-    ind = [ind_all[(k*ncut+1):((k+1)*ncut)] for k = 0:(ndiv-1)]
-
-    # iterate over divisions
-    for i = 1:ndiv
-        println("batch $i")
-        # split train/test sets
-        train_ind = reduce(vcat, ind[Not(i)])
-        ds_train = ds[train_ind]
-
-        # train using dpp
-        lb, cond_num[i], dpp_ind = train_potential(ds_train, ace, dpp_batch)
-        sel_ind[i] = train_ind[dpp_ind]
-
-        # get predicted energies
-        e_pred = get_all_energies(ds, lb)
-        f_pred = get_all_forces_mag(ds, lb)
-        e_err[:,i] = energies - e_pred 
-        f_err[:,i] = forces - f_pred
-
-        for j = 1:nfile
-            e_mae[file_arr[j]][i], e_rmse[file_arr[j]][i], _ = calc_metrics(energies[confs_id[j]], e_pred[confs_id[j]])
-            f_mae[file_arr[j]][i], f_rmse[file_arr[j]][i], _ = calc_metrics(forces[confs_id[j]], f_pred[confs_id[j]])
-        end
-    end
-
-    # populate DataFrame
-    df_conf = DataFrame("config" => 1:length(ds),
-            "file" => reduce(vcat, [[file_arr[j] for i = 1:length(confs_arr[j])] for j = 1:nfile]),
-            "DFT energy" => energies,
-            "energy err mean" => mean(e_err, dims=2)[:], # mean error from k-fold CV
-            "energy err std" => std(e_err, dims=2)[:], # std of error
-            "DFT force" => forces,
-            "force err mean" =>  mean(f_err, dims=2)[:], # mean error from k-fold CV
-            "force err std" => std(f_err, dims=2)[:], # std of error
-    )
-
-    df_meta = DataFrame("file" => file_arr,
-            "# configs" => [length(conf) for conf in confs_id],
-            "E mae mean" => [mean(e_mae[k]) for k in keys(e_mae)],
-            "E mae std" => [std(e_mae[k]) for k in keys(e_mae)],
-            "E rmse mean" => [mean(e_rmse[k]) for k in keys(e_rmse)],
-            "E rmse std" => [std(e_rmse[k]) for k in keys(e_rmse)],
-            "F mae mean" => [mean(f_mae[k]) for k in keys(f_mae)],
-            "F mae std" => [std(f_mae[k]) for k in keys(f_mae)],
-            "F rmse mean" => [mean(f_rmse[k]) for k in keys(f_rmse)],
-            "F rmse std" => [std(f_rmse[k]) for k in keys(f_rmse)],
-    )
-
-    # write to file
-    CSV.write(outpath*"$(elname)_ACE-$(nbody)-$(deg)_train_full_nDPP=$(dpp_batch).csv", df_conf)
-    CSV.write(outpath*"$(elname)_ACE-$(nbody)-$(deg)_train_metadata_nDPP=$(dpp_batch).csv", df_meta)
-
-    return sel_ind, cond_num
-end
-
-
-# function get_all_forces_mag(ds::DataSet)
-    #     for c in ds
-    #         force_coord = reduce(hcat, get_values(get_forces(c)))'
-    #         sum(force_coord, dims=1)
-
-
-
-function get_all_forces_mag(
-    ds::DataSet,
-    lb::PotentialLearning.LinearBasisPotential
-)
-    force_descriptors = [reduce(vcat, get_values(get_force_descriptors(dsi)) ) for dsi in ds]
-    force_pred = [lb.β0[1] .+  dB' * lb.β for dB in [reduce(hcat, fi) for fi in force_descriptors]]
-    return [sum(norm.([f[k:k+2] for k = 1:3:length(f)])) for f in force_pred]
 end
