cf631e58b8267b05dcbc76e561052c42edb06655
Emmanuel Lujan <lujan.emmanuel@gmail.com>
2023-12-24 00:22:22 -0500

    IP compat: update of data structures.

--- src/Data/data.jl
+++ src/Data/data.jl
@@ -1,32 +1,41 @@
 using Unitful, UnitfulAtomic, AtomsBase, ProgressBars
-import InteratomicBasisPotentials: compute_local_descriptors, compute_force_descriptors
+import InteratomicPotentials: compute_local_descriptors, compute_force_descriptors
 
 include("datatypes.jl")
 include("configs.jl")
 include("utils.jl")
 
-export Data,
-    ConfigurationData,
-    AtomicData,
-    Energy,
-    Force,
-    Forces,
-    LocalDescriptor,
-    LocalDescriptors,
-    ForceDescriptor,
-    ForceDescriptors
-export ConfigurationDataSet,
-    Configuration,
-    get_data,
-    get_system,
-    get_positions,
-    get_energy,
-    get_descriptors,
-    get_forces,
-    get_force_descriptors,
-    DataBase,
-    DataSet,
-    get_values
-export get_batch, get_energies, get_local_descriptors, get_forces, get_force_descriptors
-export compute_local_descriptors, compute_force_descriptors
+export  Data,
+        ConfigurationData,
+        AtomicData,
+        Energy,
+        Force,
+        Forces,
+        LocalDescriptor,
+        LocalDescriptors,
+        ForceDescriptor,
+        ForceDescriptors
+export  ConfigurationDataSet,
+        Configuration,
+        get_data,
+        get_system,
+        get_positions,
+        get_energy,
+        get_descriptors,
+        get_forces,
+        get_force_descriptors,
+        DataBase,
+        DataSet,
+        get_values,
+        get_all_energies,
+        get_all_forces,
+        potential_energy,
+        force,
+        compute_local_descriptors,
+        compute_force_descriptors
+export  get_batch,
+        get_energies,
+        get_local_descriptors,
+        get_forces,
+        get_force_descriptors
 
