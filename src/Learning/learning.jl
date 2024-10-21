fc1b05ab7e57eb41d5a72505a66a5e631db87644
Emmanuel Lujan <lujan.emmanuel@gmail.com>
2024-10-21 14:48:23 -0400

    Update learning.jl: export ooc_learn!

--- src/Learning/learning.jl
+++ src/Learning/learning.jl
@@ -1,6 +1,6 @@
 abstract type AbstractLearningProblem end
 
-export learn!, get_all_energies, get_all_forces
+export learn!, ooc_learn!, get_all_energies, get_all_forces
 
 include("general-learning-problem.jl")
 include("linear-learning-problem.jl")
